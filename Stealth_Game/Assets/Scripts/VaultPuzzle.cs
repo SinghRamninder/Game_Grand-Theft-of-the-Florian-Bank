de20cb0da87800d750f0bc0740a111859b7f6cc4
Ramninder <ramninder@ue-germany.de>
2026-01-04 18:47:55 +0100

    Vault door puzzle created (in test scene)

---
+++ Stealth_Game/Assets/Scripts/VaultPuzzle.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class VaultPuzzle : MonoBehaviour
+{
+    [SerializeField] private float normalSpeed = 5f;
+    [SerializeField] private float speedIncrement = 5f;
+
+    [SerializeField] private TMP_Text numbersDisplay;
+
+    private float speed;
+
+    private float angle;
+
+    private List<int> randomNumbers = new List<int>();
+
+    private int currentNumber;
+    private int roundNumber;
+
+    private void Start()
+    {
+        GenerateNumbers();
+        speed = normalSpeed;
+    }
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+        angle = angle % 360;
+
+        transform.rotation = Quaternion.Euler(0,0,angle);
+
+        if (roundNumber < 4)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                int calculatedNumber = Mathf.RoundToInt(angle / 36f);
+                calculatedNumber = ((calculatedNumber % 10) + 10) % 10;
+                if (calculatedNumber == currentNumber)
+                {
+                    roundNumber += 1;
+                    if (roundNumber == 4) return;
+
+                    speed = -(Mathf.Abs(speed) + speedIncrement) * Mathf.Sign(speed);
+                    currentNumber = randomNumbers[roundNumber - 1];
+
+                }
+                else
+                {
+                    speed = normalSpeed;
+                    Debug.LogWarning($"Calculated number: {calculatedNumber} && Current number: {currentNumber}");
+                    GenerateNumbers();
+                }
+            }
+        }
+        else
+        {
+            speed = 0f;
+            Debug.Log("Puzzle cleared");
+        }
+        
+    }
+
+    private void GenerateNumbers()
+    {
+        randomNumbers.Clear();
+        numbersDisplay.text = string.Empty;
+
+        for (int i = 0; i < 3; i++)
+        {
+            int generatedNumber = Random.Range(0, 10);
+            numbersDisplay.text += "\u00A0" + generatedNumber;
+            randomNumbers.Add(generatedNumber);
+        }
+
+        currentNumber = randomNumbers[0];
+        roundNumber = 1;
+    }
+}
